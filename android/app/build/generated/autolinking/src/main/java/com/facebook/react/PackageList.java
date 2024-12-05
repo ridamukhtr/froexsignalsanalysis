6e2aa6c03dd589b1d8a382fcdb0d0d1699cc2d50
ridamukhtr <134594152+ridamukhtr@users.noreply.github.com>
2024-12-05 14:04:36 +0500

    npms

--- android/app/build/generated/autolinking/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/autolinking/src/main/java/com/facebook/react/PackageList.java
@@ -10,7 +10,20 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-
+// @react-native-masked-view/masked-view
+import org.reactnative.maskedview.RNCMaskedViewPackage;
+// react-native-gesture-handler
+import com.swmansion.gesturehandler.RNGestureHandlerPackage;
+// react-native-pager-view
+import com.reactnativepagerview.PagerViewPackage;
+// react-native-reanimated
+import com.swmansion.reanimated.ReanimatedPackage;
+// react-native-safe-area-context
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+// react-native-screens
+import com.swmansion.rnscreens.RNScreensPackage;
+// react-native-svg
+import com.horcrux.svg.SvgPackage;
 
 public class PackageList {
   private Application application;
@@ -55,7 +68,14 @@ public class PackageList {
 
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
-      new MainReactPackage(mConfig)
+      new MainReactPackage(mConfig),
+      new RNCMaskedViewPackage(),
+      new RNGestureHandlerPackage(),
+      new PagerViewPackage(),
+      new ReanimatedPackage(),
+      new SafeAreaContextPackage(),
+      new RNScreensPackage(),
+      new SvgPackage()
     ));
   }
 }
