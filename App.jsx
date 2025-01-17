ce689d476beceba7b05ccdffbd78e0efa71bed7e
ridamukhtr <134594152+ridamukhtr@users.noreply.github.com>
2025-01-17 18:07:40 +0500

    review code

--- App.jsx
+++ App.jsx
@@ -1,7 +1,9 @@
 // import packeges
 import { StyleSheet, View } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
+import { LogLevel, OneSignal } from 'react-native-onesignal';
 // import store
 import { store } from './src/redux/store';
 // import navigator
@@ -14,6 +16,22 @@ const App = () => {
 
 	// const appId = "ae8020dd-afb4-49c2-9ec5-a8003e99b36a"
 
+	// OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+	// // OneSignal Initialization
+	// OneSignal.initialize("ae8020dd-afb4-49c2-9ec5-a8003e99b36a");
+
+	// // requestPermission will show the native iOS or Android notification permission prompt.
+	// // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+	// OneSignal.Notifications.requestPermission(true);
+
+	// // Method for listening for notification clicks
+	// OneSignal.Notifications.addEventListener('click', (event) => {
+	// 	console.log('OneSignal: notification clicked:', event);
+	// });
+	useEffect(() => {
+		SplashScreen.hide();
+	}, []);
 	return (
 		<Provider store={store}>
 			<View style={{ flex: 1, }}>
